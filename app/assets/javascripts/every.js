a6a4f5c13faf3bd984f74e8a5a25821a9ca1cfce
hwchan91 <hwchan91@gmail.com>
2017-11-24 23:27:30 +0800

    try to prevent overscrolling 5

--- app/assets/javascripts/every.js
+++ app/assets/javascripts/every.js
@@ -344,31 +344,11 @@ function everyFunction() {
   loopSoundcloud();
 
   //prevent overscrolling
-  // Uses document because document will be topmost level in bubbling
-  $(document).on('touchmove',function(e){
-    e.preventDefault();
+  $(document).bind('touchmove', function(e){
+    e.preventDefault();           
   });
 
-  var scrolling = false;
-
-  // Uses body because jquery on events are called off of the element they are
-  // added to, so bubbling would not work if we used document instead.
-  $('body').on('touchstart','.content',function(e) {
-
-      // Only execute the below code once at a time
-      if (!scrolling) {
-          scrolling = true;   
-          if (e.currentTarget.scrollTop === 0) {
-            e.currentTarget.scrollTop = 1;
-          } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-            e.currentTarget.scrollTop -= 1;
-          }
-          scrolling = false;
-      }
-  });
-
-  // Prevents preventDefault from being called on document if it sees a scrollable div
-  $('body').on('touchmove','.content',function(e) {
+  $('.content').bind('touchmove', function(e){
     e.stopPropagation();
   });
 
