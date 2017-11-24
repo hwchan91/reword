486220c53f2f2167efa3d4ca2bde4a0d64fc92ba
hwchan91 <hwchan91@gmail.com>
2017-11-24 23:21:20 +0800

    try to prevent overscrolling 3

--- app/assets/javascripts/every.js
+++ app/assets/javascripts/every.js
@@ -344,30 +344,32 @@ function everyFunction() {
   loopSoundcloud();
 
   //prevent overscrolling
-  // var selScrollable = '.scrollable';
-  // // Uses document because document will be topmost level in bubbling
-  // $(document).on('touchmove',function(e){
-  //   e.preventDefault();
-  // });
-  // // Uses body because jQuery on events are called off of the element they are
-  // // added to, so bubbling would not work if we used document instead.
-  // $('body').on('touchstart', selScrollable, function(e) {
-  //   if (e.currentTarget.scrollTop === 0) {
-  //     e.currentTarget.scrollTop = 1;
-  //   } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-  //     e.currentTarget.scrollTop -= 1;
-  //   }
-  // });
-  // // Stops preventDefault from being called on document if it sees a scrollable div
-  // $('body').on('touchmove', selScrollable, function(e) {
-  //   e.stopPropagation();
-  // });
-  $('body').on('touchmove', selScrollable, function(e) {
-    // Only block default if internal div contents are large enough to scroll
-    // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
-    if($(this)[0].scrollHeight > $(this).innerHeight()) {
-        e.stopPropagation();
-    }
+  // Uses document because document will be topmost level in bubbling
+  $(document).on('touchmove',function(e){
+    e.preventDefault();
+  });
+
+  var scrolling = false;
+
+  // Uses body because jquery on events are called off of the element they are
+  // added to, so bubbling would not work if we used document instead.
+  $('body').on('touchstart','.scrollable',function(e) {
+
+      // Only execute the below code once at a time
+      if (!scrolling) {
+          scrolling = true;   
+          if (e.currentTarget.scrollTop === 0) {
+            e.currentTarget.scrollTop = 1;
+          } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+            e.currentTarget.scrollTop -= 1;
+          }
+          scrolling = false;
+      }
+  });
+
+  // Prevents preventDefault from being called on document if it sees a scrollable div
+  $('body').on('touchmove','.scrollable',function(e) {
+    e.stopPropagation();
   });
 
 }
