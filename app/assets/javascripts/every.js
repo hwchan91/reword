74092812ec11d864fe68ec82e5338552a1ce8c45
hwchan91 <hwchan91@gmail.com>
2017-11-24 23:17:18 +0800

    try to prevent overscrolling 2

--- app/assets/javascripts/every.js
+++ app/assets/javascripts/every.js
@@ -344,23 +344,30 @@ function everyFunction() {
   loopSoundcloud();
 
   //prevent overscrolling
-  var selScrollable = '.scrollable';
-  // Uses document because document will be topmost level in bubbling
-  $(document).on('touchmove',function(e){
-    e.preventDefault();
-  });
-  // Uses body because jQuery on events are called off of the element they are
-  // added to, so bubbling would not work if we used document instead.
-  $('body').on('touchstart', selScrollable, function(e) {
-    if (e.currentTarget.scrollTop === 0) {
-      e.currentTarget.scrollTop = 1;
-    } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-      e.currentTarget.scrollTop -= 1;
-    }
-  });
-  // Stops preventDefault from being called on document if it sees a scrollable div
+  // var selScrollable = '.scrollable';
+  // // Uses document because document will be topmost level in bubbling
+  // $(document).on('touchmove',function(e){
+  //   e.preventDefault();
+  // });
+  // // Uses body because jQuery on events are called off of the element they are
+  // // added to, so bubbling would not work if we used document instead.
+  // $('body').on('touchstart', selScrollable, function(e) {
+  //   if (e.currentTarget.scrollTop === 0) {
+  //     e.currentTarget.scrollTop = 1;
+  //   } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+  //     e.currentTarget.scrollTop -= 1;
+  //   }
+  // });
+  // // Stops preventDefault from being called on document if it sees a scrollable div
+  // $('body').on('touchmove', selScrollable, function(e) {
+  //   e.stopPropagation();
+  // });
   $('body').on('touchmove', selScrollable, function(e) {
-    e.stopPropagation();
+    // Only block default if internal div contents are large enough to scroll
+    // Warning: scrollHeight support is not universal. (https://stackoverflow.com/a/15033226/40352)
+    if($(this)[0].scrollHeight > $(this).innerHeight()) {
+        e.stopPropagation();
+    }
   });
 
 }
