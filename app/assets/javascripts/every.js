ad1f9c3fac77fdfbce822d379b1e3a998a909a7c
hwchan91 <hwchan91@gmail.com>
2017-11-24 23:52:45 +0800

    prevent orientation2?

--- app/assets/javascripts/every.js
+++ app/assets/javascripts/every.js
@@ -343,14 +343,6 @@ function everyFunction() {
   }
   loopSoundcloud();
 
-  //prevent orienting
-  $(document).ready(function () {
-    function reorient(e) {
-      var portrait = (window.orientation % 180 == 0);
-      $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
-    }
-    window.onorientationchange = reorient;
-    window.setTimeout(reorient, 0);
-  });
+
 
 }
