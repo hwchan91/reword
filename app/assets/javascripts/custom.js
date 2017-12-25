32b196ee92103030bd8e7c8039f18a75302424be
hwchan91 <hwchan91@gmail.com>
2017-12-25 17:38:04 +0800

    put toggle player function outside ready

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,9 @@
 $(document).on('turbolinks:load', function() {
   everyFunction();
 })
+
+function togglePlayer() {
+  var soundcloud_elem = $('#sc_player')[0];
+  var soundcloud_player = SC.Widget(soundcloud_elem);
+  soundcloud_player.toggle();
+}
