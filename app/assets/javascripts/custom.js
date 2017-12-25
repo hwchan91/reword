1a464282e0f702bd86f60d651069af96590c5535
hwchan91 <hwchan91@gmail.com>
2017-12-25 17:47:55 +0800

    fix start pause sc

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,14 +2,14 @@ $(document).on('turbolinks:load', function() {
   everyFunction();
 })
 
-
-var soundcloud_elem = $('#sc_player')[0];
-var soundcloud_player = SC.Widget(soundcloud_elem);
-
 function startPlayer() {
-  soundcloud_player.play();
+  var soundcloud_elem = $('#sc_player')[0];
+  var soundcloud_player = SC.Widget(soundcloud_elem);
+  soundcloud_player.start();
 }
 
 function pausePlayer() {
+  var soundcloud_elem = $('#sc_player')[0];
+  var soundcloud_player = SC.Widget(soundcloud_elem);
   soundcloud_player.pause();
 }
